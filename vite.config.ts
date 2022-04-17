0ace38591760ac04e78f59089e56b967b50d1d24
Omer <27678413+TheOmer77@users.noreply.github.com>
2022-04-17 15:49:56 +0300

    Add ESLint & pretter

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { linterPlugin, EsLinter, TypeScriptLinter } from 'vite-plugin-linter';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(configEnv => ({
+  plugins: [
+    react(),
+    linterPlugin({
+      include: ['./src/**/*.ts', './src/**/*.tsx'],
+      linters: [new EsLinter({ configEnv }), new TypeScriptLinter()],
+    }),
+  ],
   server: { port: 4000, host: true },
-});
+}));
